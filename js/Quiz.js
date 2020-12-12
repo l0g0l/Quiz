629aab33afa69e85e07a7db8fffb305fe8db1041
Lucia <luciag.lara@gmail.com>
2020-12-12 17:21:28 +0100

    mejora de Alex, intento colores, no funciona

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,102 +1,66 @@
 const respuestas = { // creo esta constante y añado SOLO las respuestas correctas
     r1: "huerto",
-    r2:"azadón",
+    r2: "azadón",
     r3: "tomates",
-    r4:"ajo",
-    r5:"allegados"
-  
+    r4: "ajo",
+    r5: "allegados"
+
 };
 
-
-
-document.getElementById("formulario").addEventListener("submit", function(event){
+document.getElementById("formulario").addEventListener("submit", function (event) {
     event.preventDefault()
 
-let todo =document.getElementsByName("pregunta1");  // si pones esto en consola, ves que es un array
-let aciertos=0; // let para conteo de aciertos y presentar en consola
-let errores=0; // let para conteo de aciertos y presentar en consola
-let contestadas = 0; // let para confirmar que se contestan todas las preguntas, sino saldrá un alert
 
-for(i=0; i<todo.length;i++) {  // todo.length, itera sobre todos los parámetros que contenga TODO
-    if(todo[i].checked==true) { // Al seer TODO un array, la i va iterando en cada acción comprobando (con checked) si se ha marcado el botón de respuesta, por eso es = a true
-        contestadas=contestadas +1; // esta let sirve como conteo de si se ha contestado, ya que entra en el bucle. Al final let contestadas tiene que ser 5 (nº de preguntas que hay), sino salta alert
-        if(todo[i].value == respuestas.r1){ // la i va iterando en cada acción comprobando (con value) si el valor es igual que la respuesta de CONST RESPUESTAS
-            aciertos =aciertos +1 // esta let sirve como conteo de los aciertos 
-            console.log("Perfecto, has acertado la pregunta 1!")
-        }else { errores=errores +1 // esta let sirve como conteo de los errores
-        console.log("Ohhh, esta no era la respuesta correcta para la pregunta 1")
+    let aciertos = 0; // let para conteo de aciertos y presentar en consola
+    let errores = 0; // let para conteo de aciertos y presentar en consola
+    let contestadas = 0; // let para confirmar que se contestan todas las preguntas, sino saldrá un alert
+
+    function chequea(num, respuesta) {
+        let todo = document.getElementsByName("pregunta" + num);
+
+        console.log(todo);
+
+
+        for (i = 0; i < todo.length; i++) {  // todo.length, itera sobre todos los parámetros que contenga TODO
+            if (todo[i].checked == true) { // Al seer TODO un array, la i va iterando en cada acción comprobando (con checked) si se ha marcado el botón de respuesta, por eso es = a true
+                contestadas = contestadas + 1; 
+                
+                console.log(todo[i]);// esta let sirve como conteo de si se ha contestado, ya que entra en el bucle. Al final let contestadas tiene que ser 5 (nº de preguntas que hay), sino salta alert
+                if (todo[i].value == respuesta) { // la i va iterando en cada acción comprobando (con value) si el valor es igual que la respuesta de CONST RESPUESTAS
+                    aciertos = aciertos + 1; // esta let sirve como conteo de los aciertos 
+                    if (todo[i].value == respuesta) {
+                        todo[i].style.color = "#5ed367";
+
+                    } else { todo[i].style.color = "#ff0000"; }
+                    // esta let sirve como conteo de los errores
+                } else {errores = errores + 1}
+                
+
+            }
+            
         }
         
-    }
-}
-
-todo= document.getElementsByName("pregunta2");
-
-for(i=0; i<todo.length;i++) {
-    if(todo[i].checked==true) {
-        contestadas=contestadas +1;
-        if(todo[i].value == respuestas.r2){
-            aciertos =aciertos +1
-            console.log("Perfecto, has acertado la pregunta 2!")
-        }else { errores=errores +1
-            console.log("Ohhh, esta no era la respuesta correcta para la pregunta 2")
-        }
 
     }
-}
+    chequea(1, respuestas.r1);
+    chequea(2, respuestas.r2);
+    chequea(3, respuestas.r3);
+    chequea(4, respuestas.r4);
+    chequea(5, respuestas.r5);
 
-todo= document.getElementsByName("pregunta3");
-
-for(i=0; i<todo.length;i++) {
-    if(todo[i].checked==true) {
-        contestadas=contestadas +1;
-        if(todo[i].value == respuestas.r3){
-            aciertos =aciertos +1
-            console.log("Perfecto, has acertado la pregunta 3!")
-        }else { errores=errores +1
-            console.log("Ohhh, esta no era la respuesta correcta para la pregunta 3")
-        }
-
+    if (contestadas != 5) { // if fuera del for porque es solo para comparar el num de respuestas dadas (let CONTESTADAS) con 5 que son el total de preguntas formuladas, si no son 5, salta alert
+        alert("Tienes que contestar todas las preguntas");
+        
     }
-}
-todo= document.getElementsByName("pregunta4");
+    alert("El total de respuestas acertadas han sido: " + aciertos + "  " +
+        "El total de respuestas erróneas han sido: " + errores);
 
 
-for(i=0; i<todo.length;i++) {
-    if(todo[i].checked==true) {
-        contestadas=contestadas +1;
-        if(todo[i].value == respuestas.r4){
-            aciertos =aciertos +1
-            console.log("Perfecto, has acertado la pregunta 4!")
-        }else { errores=errores +1
-            console.log("Ohhh, esta no era la respuesta correcta para la pregunta 4")}
-
-    }
-}
-todo= document.getElementsByName("pregunta5");
-
-
-
-for(i=0; i<todo.length;i++) {
-    if(todo[i].checked==true) {
-        contestadas=contestadas +1;
-        if(todo[i].value == respuestas.r5){
-            aciertos =aciertos +1
-            console.log("Perfecto, has acertado la pregunta 5!")
-        }else { errores=errores +1
-            console.log("Ohhh, esta no era la respuesta correctaa para la pregunta 5")}
-
-    }
-}
-
-if (contestadas != 5){ // if fuera del for porque es solo para comparar el num de respuestas dadas (let CONTESTADAS) con 5 que son el total de preguntas formuladas, si no son 5, salta alert
-    alert("Tienes que contestar todas las preguntas");
-}
-
-console.log("El total de respuestas acertadas han sido: " +aciertos);
-console.log("El total de respuestas erróneas han sido: " +errores);
 });
 
 
- 
+  
+
+
+
 
